82ac06fd3ce491c0af925dc9b3c05b249cae0213
Chia Amisola <chiamisola@gmail.com>
2021-05-11 08:05:41 -0400

    Add 20 questions

--- questions.js
+++ questions.js
@@ -80,4 +80,114 @@ var questions = [
     correctChoice: "oil"
   },
 
+  {
+    question: "How many languages are written right to left?",
+    choices: ["15", "12"],
+    correctChoice: "12"
+  },
+  {
+    question: "Which country invented ice cream?",
+    choices: ["Italy", "China"],
+    correctChoice: "China"
+  },
+  {
+    question: "In which game was Waluigi first introduced?",
+    choices: ["Mario Party", "Mario Tennis"],
+    correctChoice: "Mario Tennis"
+  },
+  {
+    question: "What mammal can go the longest without water?",
+    choices: ["Camel", "Giraffe"],
+    correctChoice: "Giraffe"
+  },
+  {
+    question: "What is the 100th digit of Pi?",
+    choices: ["9", "4"],
+    correctChoice: "9"
+  },
+  {
+    question: "Which is the largest freshwater lake in the world?",
+    choices: ["Caspian Sea", "Caspian Sea"],
+    correctChoice: "Lake Superior"
+  },
+  {
+    question: "Who is on the cover of Sufjan Stevens' Carrie and Lowel?",
+    choices: ["His parents", "His idols"],
+    correctChoice: "His parents"
+  },
+  {
+    question: "In which Adam Sandler movie does Bob Barker make a cameo?",
+    choices: ["Happy Gilmore", "Billy Madison"],
+    correctChoice: "Happy Gilmore"
+  },
+  {
+    question: "In 'Billy Madison,' who plays the bus driver?",
+    choices: ["Tom Arnold", "Chris Farley"],
+    correctChoice: "Chris Farley"
+  },
+  {
+    question: "Which band has the single most sold album of all time?",
+    choices: ["Eagles", "AC/DC"],
+    correctChoice: "Eagles"
+  },
+  {
+    question: "Who was the very first American Idol winner?",
+    choices: ["Adam Lambert", "Kelly Clarkson"],
+    correctChoice: "Kelly Clarkson"
+  },
+  {
+    question: "What letter does every odd number have in it?",
+    choices: ["E", "I"],
+    correctChoice: "E"
+  },
+  {
+    question: "What is the mathematical name for a pound sign (#)?",
+    choices: ["Octothorpe", "Pound"],
+    correctChoice: "Octothorpe"
+  },
+  {
+    question: "What is the billionth digit of Pi?",
+    choices: ["9", "4"],
+    correctChoice: "9"
+  },
+  {
+    question: "What mathematician published more papers in his lifetime than any other mathematician in history?",
+    choices: ["Paul Erdos", "Leonhard Euler"],
+    correctChoice: "Paul Erdos"
+  },
+  {
+    question: "What year did the Cold War officially end?",
+    choices: ["1989", "1991"],
+    correctChoice: "1989"
+  },
+  {
+    question: "When was the idea of the atom first introduced in historical records?",
+    choices: ["450 B.C.", "500 B.C."],
+    correctChoice: "450 B.C."
+  },
+  {
+    question: "How many calories per gram are stored in protein?",
+    choices: ["3", "4"],
+    correctChoice: "4"
+  },
+  {
+    question: "What is a tall chef’s hat called?",
+    choices: ["Trilby", "Toque"],
+    correctChoice: "Toque"
+  },
+  {
+    question: "When was Skull and Bones founded?",
+    choices: ["1832", "1841"],
+    correctChoice: "1832"
+  },
+  {
+    question: "What is the most expensive book ever sold?",
+    choices: ["Codex Leicester", "Birds of America"],
+    correctChoice: "Codex Leicester"
+  },
+  {
+    question: "When was the Yale Daily News foundedd?",
+    choices: ["1878", "1876"],
+    correctChoice: "1878"
+  }
 ]
